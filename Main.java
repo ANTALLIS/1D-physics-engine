0f6fcd74506189f9490359b6608269a6ddae46c8
Antonio <antonioallis@hotmail.co.uk>
2019-05-02 18:18:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,13 +6,27 @@ public class Main {
 	}
 }
 
-class ball {
+class Ball {
 	String name;
 	float mass;
 	float radius;
 	float pos_x;
+	float vel_x;
+	float acc_x;
 	
+	Ball(String n, float m, float r, float a, float p, float v, float a) {
+		this.setBallProperties(string n, float m, float r, float a, float p, float v, float a)
+	}
 	
+	setBallProperties(string n, float m, float r, float a, float p, float v, float a) {
+		// Setter
+		name = n;
+		mass = m;
+		radius = r;
+		pos_x = p;
+		vel_x = v;
+		acc_x = a;
+	}
 }
 
 
